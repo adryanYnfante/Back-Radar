e44a7fea3e822b80880204695b234811427545ff
juandavidamayaardila <juandavidamayaardila@gmail.com>
2022-08-09 17:55:48 -0500

    se crea ejemplo de consumo de api externa

--- src/main/java/org/sofka/radar/controller/LeagueController.java
+++ src/main/java/org/sofka/radar/controller/LeagueController.java
@@ -1,13 +1,19 @@
 package org.sofka.radar.controller;
 
+import org.sofka.radar.document.RadarDocument;
+import org.sofka.radar.model.KnowlegdeArea;
+import org.sofka.radar.model.Radar;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.Locale;
 import java.util.stream.Collectors;
 
 @RestController
@@ -22,20 +28,20 @@ public class LeagueController {
         return Flux.just(coutries);
     }
 
-    @GetMapping("/searchleague")
-    private Flux<Object> getLeagueById(String idLeague){
-        String uri = "http://localhost:3000/radare/"+idLeague;
+    @GetMapping("/searchleague/{id}")
+    private Flux<Object> getLeagueById(@PathVariable String id){
+        String uri = "http://localhost:3000/radar/"+id;
         RestTemplate restTemplate = new RestTemplate();
-        Object[] radarList = restTemplate.getForObject(uri,Object[].class);
+        Object radarList = restTemplate.getForObject(uri,Object.class);
         return Flux.just(radarList);
     }
 
     @GetMapping("/searchstudent")
-    private List<Object> getLeagueByIdLeagueByIdStudent(String idLeague, String idStudent){
-        String uri = "https://restcountries.com/v2/name/argentina?fullText=true";
+    private Mono<Object> getLeagueByIdLeagueByIdStudent(String idLeague, String idStudent){
+        String uri = "http://localhost:3000/radar/";
         RestTemplate restTemplate = new RestTemplate();
-        Object[] coutries = restTemplate.getForObject(uri,Object[].class);
-        return Arrays.asList(coutries);
+        RadarDocument[] radarList = restTemplate.getForObject(uri,RadarDocument[].class);
+        return Mono.just(radarList);
     }
 
 
