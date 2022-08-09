28387273cd2a78b92e48a17e3682ceca3521d237
erickdiaz01 <erdiazb@unal.edu.co>
2022-08-09 14:24:33 -0500

    Creacion del crud de los radares

--- src/main/java/org/sofka/radar/document/RadarDocument.java
+++ src/main/java/org/sofka/radar/document/RadarDocument.java
@@ -3,8 +3,12 @@ package org.sofka.radar.document;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.sofka.radar.model.KnowlegdeArea;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Document(collection = "Radar")
 @Data
@@ -14,5 +18,11 @@ public class RadarDocument {
     @Id
     private String identification;
     private String name;
+    private List<KnowlegdeArea> knowlegdeAreas;
 
+    public RadarDocument(String identification,String name){
+        this.identification=identification;
+        this.name=name;
+        this.knowlegdeAreas= new ArrayList<>();
+    }
 }
